81a55a3fc90c202a6efd8f868cc7dc908890d49b
Dan Iverson <dan@daniverson.com>
2022-11-23 15:54:54 -0600

    Pausing to test with Click instead of Docopts

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
+HISTORY = (HERE / "HISTORY.md").read_text()
+
+setup_args = dict(
+    name='byop',
+    version='0.0.1',
+    description='psadmin.io - Bring Your Own Patches IDPK Builder',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    url='https://github.com/psadmin-io/io-byop',
+    
+    author='psadmin.io',
+    author_email='info@psadmin.io',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    keywords=['PeopleSoft', 'PeopleTools', 'CPU', 'Security Patches', 'Infrastructure DPK', 'IDPK'],    
+    include_package_data=True,
+    install_requires=["docopt>=0.6.2","requests"],
+    entry_points={
+        "console_scripts": [
+            "byop=byop.__main__:main",
+        ]
+    },
+)
+
+if __name__ == '__main__':
+    setup(**setup_args) 
