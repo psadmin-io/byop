ac5b25a08824064eab5ec6694a06a7ddf881c967
Dan Iverson <dan@daniverson.com>
2022-12-02 10:05:46 -0600

    Working on logging for cleanup

--- byop.py
+++ byop.py
@@ -4,6 +4,7 @@ import re
 import sys
 import json
 import yaml
+import glob
 import logging
 import datetime
 import click
@@ -31,11 +32,39 @@ class Config(dict):
         with self.config.open('w') as f:
             f.write(json.dumps(self, indent=2))
 
+def verbose_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(Config)
+        state.verbose = value
+        return value
+    return click.option('--verbose',
+                        default=False,
+                        is_flag=True,
+                        help='Enable debug logging',
+                        callback=callback)(f)
+
+def quiet_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(Config)
+        state.verbose = value
+        return value
+    return click.option('--quiet', 
+                        default=False, 
+                        is_flag=True, 
+                        help="Don't print timing output",
+                        callback=callback)(f)
+
+def common_options(f):
+    f = verbose_option(f)
+    f = quiet_option(f)
+    return f
+
 pass_config = click.make_pass_decorator(Config, ensure=True)
 this = sys.modules[__name__]
 this.config = None
 this.timings = None
-patch_yml = {}
+this.codes = {}
+# patch_yml = {}
 
 JDK_PATCHES = "jdk_patches"
 JDK_PATCHES_VERSION = "jdk_patches_version"
@@ -49,6 +78,10 @@ WEBLOGIC_PATCHES = "weblogic_patches"
 WEBLOGIC_PATCHES_VERSION = "weblogic_patches_version"
 
 @click.group(no_args_is_help=True)
+# @common_options 
+#####################
+### TODO - mess with common options and getting the --verbose flag to flow into setup_logging() correct from the cli() method
+#####################
 @pass_config
 def cli(config):
     """Bring Your Own Patches - Infrastructure DPK Builder"""
@@ -68,7 +101,7 @@ def cli(config):
     if not config.get('patch_status_file'):
         this.config['patch_status_file'] = os.path.join(this.config['tmp_dir'], 'patch_status_file')
 
-    pass  
+    pass
 
 @cli.command()
 @click.option('-u', '--mos-username',
@@ -79,12 +112,61 @@ def cli(config):
               prompt=True, hide_input=True, confirmation_prompt=True)
 @pass_config
 def config(config, mos_username, mos_password):
+    """Build the config.json"""
     setup_logging()
 
     config["mos_username"] = mos_username
     config["mos_password"] = mos_password
     config.save()
     logging.info("Configuration save to config.json")
+
+@cli.command()
+@click.option('--yaml',
+              is_flag=True,
+              default=False,
+              help="Include psft_patches.yaml in cleanup")
+@click.option('--tgt-yaml', 
+              help="Target YAML file to delete. Default is psft_patches.yaml.",
+              default='psft_patches.yaml')
+@pass_config
+@common_options
+def cleanup(config, yaml, tgt_yaml, verbose, quiet):
+    """Remove files from the tmp and cpu_archives directories."""
+    this.config['verbose'] = verbose
+    this.config['quiet'] = quiet
+    setup_logging()
+
+    files = glob.glob(config.get('archive_dir') + "/*/*", recursive=True)
+    if files:
+        for f in files:
+            try:
+                os.remove(f)
+                logging.info("Removed file: " + str(f))
+            except OSError as e:
+                logging.error("Error: %s : %s" % (f, e.strerror))
+    else:
+        logging.info("No patches to cleanup")
+
+    files = glob.glob(config.get('tmp_dir') + "/*", recursive=True)
+    if files:
+        for f in files:
+            try:
+                os.remove(f)
+                logging.info("Removed file: " + str(f))
+            except OSError as e:
+                logging.error("Error: %s : %s" % (f, e.strerror))
+    else:
+        logging.info("No temporary files to cleanup")
+
+    if yaml:
+        if os.path.exists(tgt_yaml):
+            try:
+                os.remove(tgt_yaml)
+                logging.info("Removed file: " + str(tgt_yaml))
+            except OSError as e:
+                logging.error("Error: %s : %s" % (tgt_yaml, e.strerror))
+        else:
+            logging.info("No " + tgt_yaml + " file to cleanup")
 
 # ##### #
 # build #
@@ -102,16 +184,10 @@ def config(config, mos_username, mos_password):
               default=False,
               is_flag=True,
               help="Ignore patch status - force all patches to be redownloaded.")
-@click.option('--verbose', 
-              default=False, 
-              is_flag=True, 
-              help="Enable debug logging")
-@click.option('--quiet', 
-              default=False, 
-              is_flag=True, 
-              help="Don't print timing output")
+@common_options
 @pass_config
 def build(config, src_yaml, tgt_yaml, redownload, verbose, quiet):
+    """Download and create an Infra-DPK package"""
 
     this.config['redownload'] = redownload
     this.config['verbose'] = verbose
@@ -127,30 +203,7 @@ def build(config, src_yaml, tgt_yaml, redownload, verbose, quiet):
     setup_logging()
     init_timings()
     build_directories()
-
-    codes = {}
-    with open(os.path.join(os.getcwd(), 'codes', 'codes.yaml')) as c:
-        codes = yaml.load(c, Loader=yaml.FullLoader)
-
-    with open(src_yaml, 'r') as f:
-        yml = yaml.load(f, Loader=yaml.FullLoader)
-        logging.debug(yml)
-        
-        try:
-            platform = codes['platform'][yml['platform']]
-            logging.debug("Platform - " + yml['platform'] + ": " + platform)
-        except:
-            logging.error("Input YAML file must specify platform")
-
-        try:
-            release = codes['peopletools'][yml['peopletools']]['weblogic']
-            logging.debug("PeopleTools - " + yml['peopletools'] + ": " + release)
-        except:
-            logging.error("Input YAML file must specify PeopleTools")
-
-        if yml[WEBLOGIC_PATCHES_VERSION]:
-            get_weblogic_patches(yml, platform, release)
-
+    download_patches()
     print_timings()
     pass
 
@@ -162,44 +215,118 @@ def build_directories():
         os.makedirs(this.config['archive_dir'], exist_ok = True)
     except OSError as error:
         logging.error("Directory '%s' can not be created" % this.config['archive_dir'])
-    
+
     try:
         os.makedirs(os.path.join(this.config['archive_dir'], WEBLOGIC_PATCHES), exist_ok = True)
     except OSError as error:
         logging.error("Directory '%s' can not be created" % os.path.join(this.config['archive_dir'], WEBLOGIC_PATCHES))
-    
+    try:
+        os.makedirs(os.path.join(this.config['archive_dir'], WEBLOGIC_OPATCH_PATCHES), exist_ok = True)
+    except OSError as error:
+        logging.error("Directory '%s' can not be created" % os.path.join(this.config['archive_dir'], WEBLOGIC_OPATCH_PATCHES))
+    try:
+        os.makedirs(os.path.join(this.config['archive_dir'], TUXEDO_PATCHES), exist_ok = True)
+    except OSError as error:
+        logging.error("Directory '%s' can not be created" % os.path.join(this.config['archive_dir'], TUXEDO_PATCHES))
+    try:
+        os.makedirs(os.path.join(this.config['archive_dir'], JDK_PATCHES), exist_ok = True)
+    except OSError as error:
+        logging.error("Directory '%s' can not be created" % os.path.join(this.config['archive_dir'], JDK_PATCHES))
+
     try:
         os.makedirs(os.path.join(this.config['tmp_dir']), exist_ok = True)
     except OSError as error:
         logging.error("Directory '%s' can not be created" % os.path.join(this.config['tmp_dir']))
 
-def get_weblogic_patches(yml, platform, release):
+def download_patches():
+    with open(os.path.join(os.getcwd(), 'codes', 'codes.yaml')) as c:
+        this.codes = yaml.load(c, Loader=yaml.FullLoader)
+
+    with open(this.config.get('src_yaml'), 'r') as f:
+        yml = yaml.load(f, Loader=yaml.FullLoader)
+        logging.debug(json.dumps(yml, indent=2))
+
+    try:
+        platform = this.codes['platform'][yml['platform']]
+        logging.debug("Platform - " + yml['platform'] + ": " + platform)
+    except:
+        logging.error("Input YAML file must specify platform")
+        raise
+
+    try:
+        ptversion = str(yml['peopletools'])
+        logging.debug("Download patches for PeopleTools " + ptversion)
+    except:
+        logging.error("Input YAML file must specify PeopleTools")
+        raise
+
+    try:
+        if yml[WEBLOGIC_PATCHES_VERSION]:
+            release = this.codes['peopletools'][ptversion]['weblogic']
+            logging.debug("PeopleTools - " + yml['peopletools'] + ": " + release)
+            get_weblogic_patches(yml, WEBLOGIC_PATCHES_VERSION, platform, release)
+    except:
+        logging.info("No Weblogic Patches")
+
+    try:
+        if yml[WEBLOGIC_OPATCH_PATCHES]:
+            
+            release = this.codes['peopletools'][yml['peopletools']]['weblogic_opatch']
+            logging.debug("PeopleTools - " + yml['peopletools'] + ": " + release)
+            get_weblogic_opatch_patches(yml, WEBLOGIC_OPATCH_PATCHES, platform, release)
+    except:
+        logging.info("No Weblogic OPatch Patches")
+
+def get_weblogic_patches(yml, section, platform, release):
     timing_key = "weblogic patches"
     start_timing(timing_key)
     
     weblogic_patches = {}
     weblogic_patches_version = {}
 
-    logging.info("Downloading " + str(len(yml[WEBLOGIC_PATCHES_VERSION])) + " patches for Weblogic")
+    logging.info("Downloading " + str(len(yml[section])) + " patches for Weblogic")
     downloaded = False
-    for i, patch in enumerate(yml[WEBLOGIC_PATCHES_VERSION], start=1):
+    for i, patch in enumerate(yml[section], start=1):
         logging.info(" - Downloading WebLogic Patch: " + str(patch))
-        file_name = get_patch(patch, platform, release, WEBLOGIC_PATCHES)
+        file_name = get_patch(patch, platform, release, section)
         if file_name:
             downloaded = True
             weblogic_patches_version["patch" + str(i)] = str(patch)
-            weblogic_patches["patch" + str(i)] = '%{hiera("peoplesoft_base")}/dpk/cpu_archives/weblogic_patches/' + file_name
+            weblogic_patches["patch" + str(i)] = '%{hiera("peoplesoft_base")}/dpk/cpu_archives/' + section + '/' + file_name
 
     if downloaded:
         logging.debug("weblogic_patches_version: ")
         logging.debug(yaml.dump(weblogic_patches_version))
-        __write_to_yaml(weblogic_patches_version, WEBLOGIC_PATCHES_VERSION)
+        __write_to_yaml(weblogic_patches_version, section)
 
         logging.debug("weblogic_patches: ")
         logging.debug(yaml.dump(weblogic_patches))
         __write_to_yaml(weblogic_patches, WEBLOGIC_PATCHES)
 
     
+    end_timing(timing_key)
+
+def get_weblogic_opatch_patches(yml, section, platform, release):
+    timing_key = "weblogic opatch patches"
+    start_timing(timing_key)
+    
+    patches = {}
+
+    logging.info("Downloading " + str(len(yml[section])) + " patches for " + timing_key)
+    downloaded = False
+    for i, patch in enumerate(yml[section], start=1):
+        logging.info(" - Downloading Patch: " + str(patch))
+        file_name = get_patch(patch, platform, release, section)
+        if file_name:
+            downloaded = True
+            # weblogic_patches_version["patch" + str(i)] = str(patch)
+            patches["patch" + str(i)] = '%{hiera("peoplesoft_base")}/dpk/cpu_archives/' + section + '/' + file_name
+
+    if downloaded:
+        logging.debug(section + ": ")
+        logging.debug(yaml.dump(patches))
+        __write_to_yaml(patches, section)
+
     end_timing(timing_key)
 
 def get_patch(patch, platform, release, product):
@@ -212,8 +339,8 @@ def get_patch(patch, platform, release, product):
         logging.info("Patch already downloaded: " + str(patch))
         return False
 
-# Copied from ioco - thanks Kyle!
 def __get_mos_patch(patch, platform, release, product):
+    # Copied from ioco - thanks Kyle!
     logging.info(" - Downloading files from MOS")
     timing_key = "__get_mos_patch"
     start_timing(timing_key)
@@ -264,10 +391,8 @@ def __get_mos_patch(patch, platform, release, product):
         # Use same session to search for downloads
         logging.debug('Search for list of downloads, using same session')
         mos_uri_search = "https://updates.oracle.com/Orion/SimpleSearch/process_form?search_type=patch&patch_number=" + str(patch) + "&plat_lang=" + str(platform)
-        logging.debug("Search URL: " + mos_uri_search)
         r = s.get(mos_uri_search) 
         search_results = r.content.decode('utf-8')
-        logging.debug("Search Results: " + search_results)
         
         # Validate search results                 
         if r.ok:
@@ -386,27 +511,15 @@ def __update_patch_status(step, status):
     except:
         logging.error('Issue updating patch status json file')
 
-def __merge(dict_1, dict_2):
-    """Merge two dictionaries.
-    Values that evaluate to true take priority over falsy values.
-    `dict_1` takes priority over `dict_2`.
-    """
-    return dict((str(key), dict_1.get(key) or dict_2.get(key))
-        for key in set(dict_2) | set(dict_1))
-
 def __write_to_yaml(dict, header):
 
-    with open(this.config.get('tgt_yaml'), 'a+') as tgt_yaml:
+    with open(this.config.get('tgt_yaml'), 'r') as tgt_yaml:
         tgt = yaml.load(tgt_yaml, Loader=yaml.FullLoader) or {}
-        
-        # Merge with existing patches
-        # new = {}
-        # new[header] = dict
-        # merged = __merge(new, tgt)
-        # logging.debug("Merged Dictionary: " + merged)
-        tgt.pop(header, None)
-        tgt[header] = dict
+
+    tgt.pop(header, None)
+    tgt[header] = dict
     
+    with open(this.config.get('tgt_yaml'), 'w') as tgt_yaml:
         yaml.dump(tgt, tgt_yaml, sort_keys=True, indent=2)
 
 def setup_logging():
@@ -431,7 +544,7 @@ def setup_logging():
     consoleHandler.setLevel(loglevel)
     rootLogger.addHandler(consoleHandler)
 
-    logging.debug('log: byop.log')
+    logging.debug('Debug Log File: byop.log')
 
 def init_timings():
     this.timings = {}
